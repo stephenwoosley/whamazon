b4b773ebd58370a3f279f135843fdd95d0faebf4
Stephen Woosley <swoosley@sharptop.co>
2017-02-26 01:20:27 -0500

    building in command-line functionality. Capturing input with inquirer js is complete, just have to figure out how to correctly update decreasing stock quantity in the db.

--- whamazonCustomer.js
+++ whamazonCustomer.js
@@ -13,8 +13,8 @@ connection.connect(function(err){
   if(err) throw err;
   console.log("Connected as User #" + connection.threadId);
   showProductList();
+  userOrder();
 });
-
 
 function showProductList()  {
   connection.query("SELECT * FROM products", function(err, res){
@@ -42,4 +42,41 @@ function showProductList()  {
     }
     console.log("-------------------------------------------------");;
   });
+}
+
+function userOrder() {
+
+  inquirer.prompt([
+    {
+      name: "itemId",
+      type: "input",
+      message: "What is the ID of item you would like to buy?"
+    }, {
+      name: "quantity",
+      type: "input",
+      message: "How many would you like to buy?",
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    }]).then(function(answer){
+      console.log("Chosen item ID is " + answer.itemId + " & you'd like to buy " + answer.quantity + " of them.");
+      connection.query("SELECT stock_quantity FROM products WHERE item_id =" + answer.itemId, function(err, res){
+        if(err) throw err;
+        if (res[0].stock_quantity < answer.quantity) {
+          console.log("Unfortunately we don't have as many as you need! Please select a lower amount.");
+          userOrder();
+        }
+        else {
+          connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: "stock_quantity" - answer.quantity}, {item_id: answer.itemId}], function(err, res){
+            if(err) throw err;
+            console.log("Congratulations on your new purchase!!!");
+          })
+        }
+
+      });
+    });
+
 }
